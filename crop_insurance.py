fd82cd62df4c32e276567fa1f0b5a4f573475e49
Misagh95 <misaghmafi9@gmail.com>
2026-07-27 18:52:50 +0330

    fix: dataclass from standard library, not genlayer

--- crop_insurance.py
+++ crop_insurance.py
@@ -1,5 +1,6 @@
 # { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
-from genlayer import gl, allow_storage, dataclass, Address, u256, TreeMap, DynArray, UserError
+from genlayer import gl, allow_storage, Address, u256, TreeMap, DynArray, UserError
+from dataclasses import dataclass
 import json
 
 
